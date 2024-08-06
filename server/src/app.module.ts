2d7e00c9697398e01cf079d0c14e85ffa9b19439
huseong2 <huseong734@gmail.com>
2024-08-07 00:15:54 +0900

    refactor: Rename Question to Questions

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuestionModule } from './question/question.module';
+import { QuestionsModule } from './questions/questions.module';
 
 @Module({
-  imports: [QuestionModule],
+  imports: [QuestionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
