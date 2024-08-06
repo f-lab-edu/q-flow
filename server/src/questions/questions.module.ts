808789385a20cc3f6b993064d13d29f2ee83a482
huseong2 <huseong734@gmail.com>
2024-08-07 00:17:40 +0900

    feat: Add Questions Controller

--- server/src/questions/questions.module.ts
+++ server/src/questions/questions.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { QuestionsController } from './questions.controller';
 
 @Module({
-  controllers: []
+  controllers: [QuestionsController]
 })
 export class QuestionsModule {}
