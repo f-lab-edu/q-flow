4dc99b27ca4b479b5bf6075cda3be255553a6472
huseong2 <huseong734@gmail.com>
2024-09-01 18:52:20 +0900

    fix: fix questions service test code

--- server/src/questions/questions.service.spec.ts
+++ server/src/questions/questions.service.spec.ts
@@ -13,7 +13,6 @@ describe('QuestionsService', () => {
 
     service = module.get<QuestionsService>(QuestionsService);
     prismaService = module.get<PrismaService>(PrismaService);
-    await prismaService.question.deleteMany();
   });
 
   afterEach(async () => {
@@ -33,25 +32,5 @@ describe('QuestionsService', () => {
     });
     expect(savedQuestion).toBeDefined();
     expect(savedQuestion.title).toBe(title);
-  });
-
-  it('should get all questions', async () => {
-    // given
-    const title1 = 'Test Question 1';
-    await service.saveQuestion(title1);
-    const title2 = 'Test Question 2';
-    await service.saveQuestion(title2);
-    const title3 = 'Test Question 3';
-    await service.saveQuestion(title3);
-
-    // when
-    const questions = await service.getAllQuestions();
-
-    // then
-    expect(questions).toBeDefined();
-    expect(questions.length).toBe(3);
-    expect(questions[0].title).toBe(title1);
-    expect(questions[1].title).toBe(title2);
-    expect(questions[2].title).toBe(title3);
   });
 });
